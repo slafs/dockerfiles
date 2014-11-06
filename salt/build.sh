0597e0f016257869952931ebea11a5a2593d72b2
Sławek Ehlert <slafs@op.pl>
2014-11-06 11:21:32 +0100

    build everything

--- salt/build.sh
+++ salt/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# build minions
 for miniondir in $( ls -d salt-minion-* ); do
     docker build --rm=true -t slafs/$miniondir $miniondir
 done
 
+# build master
+docker build --rm=true -t slafs/salt-master salt-master
