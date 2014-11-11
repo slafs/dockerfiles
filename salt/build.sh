fef9053e582b0f4fb87ea279ff51ca7009e13e99
Sławek Ehlert <slafs@op.pl>
2014-11-11 22:26:28 +0100

    properly tag images

--- salt/build.sh
+++ salt/build.sh
@@ -5,5 +5,7 @@ for miniondir in $( ls -d salt-minion-* ); do
     docker build --rm=true -t slafs/$miniondir $miniondir
 done
 
-# build master
-docker build --rm=true -t slafs/salt-master salt-master
+# build masters
+for masterdir in $( ls -d salt-master-* ); do
+    docker build --rm=true -t slafs/$masterdir $masterdir
+done
